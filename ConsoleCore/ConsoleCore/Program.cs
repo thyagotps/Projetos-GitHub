90fd64f108b5d66b37d50dcb5d089a8302c6ef35
Thyago Pavan <thyagopavan@yahoo.com.br>
2019-12-15 21:41:32 -0300

    [NEW] Criado estrutura do projeto.

---
+++ ConsoleCore/ConsoleCore/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("dotnet Core!");
+            Console.ReadKey();
+        }
+    }
+}
