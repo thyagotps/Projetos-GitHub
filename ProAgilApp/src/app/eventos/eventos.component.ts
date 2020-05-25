3b0cd313166181ce3463b3dbe0f7a33f06ac585f
Thyago Pavan <thyagopavan@yahoo.com.br>
2020-05-25 15:51:09 -0300

    [NEW] Adicionado funcionalidade de filtro no grid de Eventos.

--- ProAgilApp/src/app/eventos/eventos.component.ts
+++ ProAgilApp/src/app/eventos/eventos.component.ts
@@ -8,10 +8,22 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosComponent implements OnInit {
 
+  private _filtroLista: string;
+  get filtroLista(): string {
+    return this._filtroLista;
+  }
+  set filtroLista(value: string) {
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+
   eventos: any = [];
+  eventosFiltrados: any = [];
   imagemLargura = 100;
   imagemMargem = 2;
   mostrarImagem = false;
+  
 
   constructor(private http: HttpClient) { }
 
@@ -27,6 +39,17 @@ export class EventosComponent implements OnInit {
 
   alternarImagem(){
     this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    );
+  }
+
+  buscarTodos(){
+    this.eventosFiltrados = this.eventos;
   }
 
 }
